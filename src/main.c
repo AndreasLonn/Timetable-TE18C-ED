943d3f2f63e6279aa4b5fd4e0e01984923999f91
AndreasLonn <andreas@loenn.se>
2019-01-31 18:26:25 +0100

    Added automatic week chooser based on system date

--- src/main.c
+++ src/main.c
@@ -2,10 +2,13 @@
 #include <graphx.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+//#include <debug.h>
 
 #define lessonWidth 64
 #define lessonHeight 32
-#define lessonHeightMargin (16 / 6)
+#define lessonHeightMargin (8 / 3)
 #define lessonBorderColor 0
 
 #define colorEngelska 230
@@ -26,18 +29,25 @@
 
 enum LessonInfo { NameLocation, TimeStartEnd, TeacherLength };
 enum LessonInfo lessonInfo = NameLocation;
-bool weekB = false;
 
 void drawLesson();
 void drawLessons();
 enum LessonInfo nextMode();
+uint8_t getWeekNumber();
+bool getWeekB();
+
+bool weekB;
 
 void main(void) {
 
     sk_key_t key;
+    uint8_t week;
 
     gfx_Begin();
+
     gfx_SetDrawBuffer();
+
+    weekB = getWeekB();
 
     drawLessons();
 
@@ -45,7 +55,7 @@ void main(void) {
 
     while((key = os_GetCSC()) != 9){
         if(key == 1 || key == 4 || key == 15 || key == 54){
-            lessonInfo = nextMode(lessonInfo, key != 1);
+            lessonInfo = nextMode(lessonInfo, key != 4);
             drawLessons();
             gfx_SwapDraw();
         } if(key == 2 || key == 3 || key == 48){
@@ -58,6 +68,17 @@ void main(void) {
     gfx_End();
 }
 void drawLessons(void){
+    uint8_t day;
+    uint8_t month;
+    uint16_t year;
+    char* dateMsg = malloc(32);
+    char* dayS = malloc(32);
+    char* monthS = malloc(32);
+    char* yearS = malloc(32);
+    char* weekS = malloc(32);
+
+    memset(dateMsg, 0, sizeof(dateMsg));
+
     gfx_FillScreen(255);
 
     gfx_SetColor(31);
@@ -78,7 +99,7 @@ void drawLessons(void){
     drawLesson(1 * lessonWidth, 4 * (lessonHeight + lessonHeightMargin), "Engelska", "B107", "jormo", "01:20", colorEngelska, textColorDark, "12:55", "14:15");
 
     //Onsdag
-    drawLesson(2 * lessonWidth, 1 * (lessonHeight + lessonHeightMargin), "Matte", weekB ? "H341" : "F206", "drada", "01:10", colorMatte, textColorDark, "09:25", "10:35");
+    drawLesson(2 * lessonWidth, 1 * (lessonHeight + lessonHeightMargin), "Matte", !weekB ? "H341" : "F206", "drada", "01:10", colorMatte, textColorDark, "09:25", "10:35");
     drawLesson(2 * lessonWidth, 2 * (lessonHeight + lessonHeightMargin), "Fysik", "F206", "drada", "00:50", colorFysik, textColorDark, "10:40", "11:30");
     drawLesson(2 * lessonWidth, 3 * (lessonHeight + lessonHeightMargin), "Lunch", "", "", "00:45", colorLunch, textColorLight, "11:30", "12:15");
 
@@ -101,8 +122,37 @@ void drawLessons(void){
     drawLesson(4 * lessonWidth, 3 * (lessonHeight + lessonHeightMargin), "Fysik", "F206", "drada", "01:10", colorFysik, textColorDark, "12:10", "13:20");
     drawLesson(4 * lessonWidth, 4 * (lessonHeight + lessonHeightMargin), "Engelska", "B107", "jormo", "01:00", colorEngelska, textColorDark, "13:40", "14:40");
     drawLesson(4 * lessonWidth, 5 * (lessonHeight + lessonHeightMargin), "Matte", "H341", "drada", "01:00", colorMatte, textColorDark, "14:50", "15:50");
+    
+    boot_GetDate(&day, &month, &year);
+    sprintf(dayS, "%d", day);
+    sprintf(monthS, "%d", month);
+    sprintf(yearS, "%d", year);
+    sprintf(weekS, "%d", getWeekNumber());
+    
+    if(weekB == getWeekB()){
+        strcat(dateMsg, dayS);
+        strcat(dateMsg, "/");
+        strcat(dateMsg, monthS);
+        strcat(dateMsg, "-");
+        strcat(dateMsg, yearS);
+        strcat(dateMsg, ", V: ");
+        strcat(dateMsg, weekS);
+        strcat(dateMsg, " : ");
+    } else
+        strcat(dateMsg, "Vecka ");
+    if(!weekB)
+        strcat(dateMsg, "A");
+    else
+        strcat(dateMsg, "B");
 
-    gfx_PrintStringXY(!weekB ? "Vecka A" : "Vecka B", LCD_WIDTH / 2 - 32, LCD_HEIGHT - lessonHeight / 2);
+    gfx_PrintStringXY(dateMsg, LCD_WIDTH / 2 - (gfx_GetStringWidth(dateMsg) / 2), LCD_HEIGHT - lessonHeight / 2);
+   
+    free(dateMsg);
+    memset(dateMsg, 0, sizeof(dateMsg));
+    free(dayS);
+    free(monthS);
+    free(yearS);
+    free(weekS);
 }
 
 void drawLesson(int16_t x, int16_t y, char *name, char *location, char *teacher, char *length, int8_t lessonColor, int8_t textColor, char *timeFrom, char *timeTo){
@@ -138,4 +188,97 @@ enum LessonInfo nextMode(enum LessonInfo currentLessonInfo, bool up) {
         default:
             return up ? NameLocation : TimeStartEnd;
     }
+}
+
+uint8_t getWeekNumber(){
+    uint8_t day;
+    uint8_t month;
+    uint16_t year;
+    boot_GetDate(&day, &month, &year);
+    //day = 1;
+    //month = 1;
+    //year = 2019;
+    if(year == 2019 && month <= 12){
+        switch(month){
+            case 12:
+                day += 30;
+            case 11:
+                day += 31;
+            case 10:
+                day += 30;
+            case 9:
+                day += 31;
+            case 8:
+                day += 31;
+            case 7:
+                day += 30;
+            case 6:
+                day += 31;
+            case 5:
+                day += 30;
+            case 4:
+                day += 31;
+            case 3:
+                day += 28;
+            case 2:
+                day += 31;
+            case 1:
+                day += 0;
+        }
+        return day / 7 + (day % 7 == 1 ? 0 : 1);
+    } else {
+        return 101;
+    }
+}
+
+bool getWeekB() {
+    switch (getWeekNumber()){
+        case 1:
+            return false;
+        case 2:
+            return true;
+        case 3:
+            return false;
+        case 4:
+            return true;
+        case 5:
+            return false;
+        case 6:
+            return true;
+        // V7 Lov
+        case 8:
+            return false;
+        case 9:
+            return true;
+        case 10:
+            return false;
+        case 11:
+            return true;
+        case 12:
+            return false;
+        case 13:
+            return true;
+        case 14:
+            return false;
+        case 15:
+            return true;
+        // V16 Lov
+        case 17:
+            return false;
+        case 18:
+            return true;
+        case 19:
+            return false;
+        case 20:
+            return true;
+        case 21:
+            return false;
+        case 22:
+            return true;
+        case 23:
+            return false;
+        case 24:
+            return true;
+    }
+    return false;
 }
