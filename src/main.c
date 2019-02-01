47f5cc081434516bfa0668cbe575388aa70e5bbd
AndreasLonn <andreas@loenn.se>
2019-02-02 00:03:01 +0100

    Added help page and some other stuff

--- src/main.c
+++ src/main.c
@@ -1,8 +1,10 @@
 #include <tice.h>
 #include <graphx.h>
 #include <stdint.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <fileioc.h>
 #include <string.h>
 //#include <debug.h>
 
@@ -27,45 +29,77 @@
 #define textColorLight 255
 #define textColorTransparent 254
 
+#define modeREAD "r"
+#define modeWRITE "w"
+
+#define classBvarName "classBvar"
+
 enum LessonInfo { NameLocation, TimeStartEnd, TeacherLength };
 enum LessonInfo lessonInfo = NameLocation;
 
-void drawLesson();
 void drawLessons();
+void drawHelp();
+void drawLesson();
 enum LessonInfo nextMode();
 uint8_t getWeekNumber();
 bool getWeekB();
 
 bool weekB;
+bool classB;
+bool showingHelp;
 
 void main(void) {
-
     sk_key_t key;
     uint8_t week;
+    ti_var_t classBvar;
 
     gfx_Begin();
 
     gfx_SetDrawBuffer();
 
+    classBvar = ti_Open(classBvarName, modeREAD);
+
     weekB = getWeekB();
+    classB = ti_GetC(classBvar);
+
+    ti_Close(classBvar);
 
     drawLessons();
 
     gfx_SwapDraw();
 
     while((key = os_GetCSC()) != 9){
-        if(key == 1 || key == 4 || key == 15 || key == 54){
+        if(showingHelp && key != 0){
+            showingHelp = false;
+            drawLessons();
+            gfx_SwapDraw();
+        } else if(key == 1 || key == 4 || key == 54){
+            showingHelp = false;
             lessonInfo = nextMode(lessonInfo, key != 4);
             drawLessons();
             gfx_SwapDraw();
-        } if(key == 2 || key == 3 || key == 48){
+        } else if(key == 2 || key == 3 || key == 48){
+            showingHelp = false;
             weekB = !weekB;
             drawLessons();
+            gfx_SwapDraw();
+        } else if(key == 15){
+            showingHelp = false;
+            classB = !classB;
+            classBvar = ti_Open(classBvarName, modeWRITE);
+            ti_PutC(classB, classBvar);
+            ti_Close(classBvar);
+            drawLessons();
+            gfx_SwapDraw();
+        } else if(key == 45){
+            showingHelp = false;
+            drawHelp();
             gfx_SwapDraw();
         }
     };
 
     gfx_End();
+    ti_CloseAll();
 }
 void drawLessons(void){
     uint8_t day;
@@ -107,21 +141,27 @@ void drawLessons(void){
     drawLesson(3 * lessonWidth, 0 * (lessonHeight + lessonHeightMargin), "Matte", "F206", "drada", "01:00", colorMatte, textColorDark, "08:00", "09:00");
     drawLesson(3 * lessonWidth, 1 * (lessonHeight + lessonHeightMargin), "Idrott", "", "tejpe", "01:30", colorIdrott, textColorDark, "09:40", "11:10");
     drawLesson(3 * lessonWidth, 2 * (lessonHeight + lessonHeightMargin), "Lunch", "", "", "00:45", colorLunch, textColorLight, "11:35", "12:20");
-    if(weekB){
+    if(weekB && !classB){
         drawLesson(3 * lessonWidth, 3 * (lessonHeight + lessonHeightMargin), "Kemi", "H324La", "altla", "01:20", colorKemi, textColorDark, "12:20", "13:40");
         drawLesson(3 * lessonWidth, 4 * (lessonHeight + lessonHeightMargin), "Teknik", "H341", "chuga", "01:30", colorTeknik, textColorLight, "14:00", "15:30");
-    } else {
+    } else if(!classB) {
         drawLesson(3 * lessonWidth, 3 * (lessonHeight + lessonHeightMargin), "Teknik", "H341", "chuga", "01:30", colorTeknik, textColorLight, "12:20", "13:50");
         drawLesson(3 * lessonWidth, 4 * (lessonHeight + lessonHeightMargin), "Fysik", "F204La", "drada", "01:20", colorFysik, textColorDark, "14:00", "15:20");
+    } else if(weekB){
+        drawLesson(3 * lessonWidth, 3 * (lessonHeight + lessonHeightMargin), "Teknik", "H341", "chuga", "01:30", colorTeknik, textColorLight, "12:20", "13:50");
+        drawLesson(3 * lessonWidth, 4 * (lessonHeight + lessonHeightMargin), "Kemi", "H324La", "altla", "01:20", colorKemi, textColorDark, "14:00", "15:20");
+    } else {
+        drawLesson(3 * lessonWidth, 3 * (lessonHeight + lessonHeightMargin), "Fysik", "F204La", "drada", "01:20", colorFysik, textColorDark, "12:20", "13:40");
+        drawLesson(3 * lessonWidth, 4 * (lessonHeight + lessonHeightMargin), "Teknik", "H341", "chuga", "01:30", colorTeknik, textColorLight, "14:00", "15:30");
     }
     drawLesson(3 * lessonWidth, 5 * (lessonHeight + lessonHeightMargin), "Resurstid", "", "", "01:00", colorResurstid, textColorLight, "15:30", "16:30");
 
     //Fredag
     drawLesson(4 * lessonWidth, 1 * (lessonHeight + lessonHeightMargin), "Kemi", "H331", "altla", "01:00", colorKemi, textColorDark, "09:50", "10:50");
     drawLesson(4 * lessonWidth, 2 * (lessonHeight + lessonHeightMargin), "Lunch", "", "", "00:45", colorLunch, textColorLight, "11:00", "11:45");
-    drawLesson(4 * lessonWidth, 3 * (lessonHeight + lessonHeightMargin), "Fysik", "F206", "drada", "01:10", colorFysik, textColorDark, "12:10", "13:20");
+    drawLesson(4 * lessonWidth, 3 * (lessonHeight + lessonHeightMargin), "Fysik", "F206", "drada", "01:20", colorFysik, textColorDark, "12:10", "13:30");
     drawLesson(4 * lessonWidth, 4 * (lessonHeight + lessonHeightMargin), "Engelska", "B107", "jormo", "01:00", colorEngelska, textColorDark, "13:40", "14:40");
-    drawLesson(4 * lessonWidth, 5 * (lessonHeight + lessonHeightMargin), "Matte", "H341", "drada", "01:00", colorMatte, textColorDark, "14:50", "15:50");
+    drawLesson(4 * lessonWidth, 5 * (lessonHeight + lessonHeightMargin), "Matte", "F203", "drada", "01:00", colorMatte, textColorDark, "14:50", "15:50");
     
     boot_GetDate(&day, &month, &year);
     sprintf(dayS, "%d", day);
@@ -135,15 +175,19 @@ void drawLessons(void){
         strcat(dateMsg, monthS);
         strcat(dateMsg, "-");
         strcat(dateMsg, yearS);
-        strcat(dateMsg, ", V: ");
+        strcat(dateMsg, ", W: ");
         strcat(dateMsg, weekS);
         strcat(dateMsg, " : ");
     } else
-        strcat(dateMsg, "Vecka ");
+        strcat(dateMsg, "Week ");
     if(!weekB)
         strcat(dateMsg, "A");
     else
         strcat(dateMsg, "B");
+    if(!classB)
+        strcat(dateMsg, ", c: A");
+    else
+        strcat(dateMsg, ", c: B");
 
     gfx_PrintStringXY(dateMsg, LCD_WIDTH / 2 - (gfx_GetStringWidth(dateMsg) / 2), LCD_HEIGHT - lessonHeight / 2);
    
@@ -153,6 +197,20 @@ void drawLessons(void){
     free(monthS);
     free(yearS);
     free(weekS);
+}
+void drawHelp(void){
+    showingHelp = true;
+    gfx_FillScreen(255);
+    gfx_PrintStringXY("Button Help", 15, 10);
+    gfx_PrintStringXY("enter - Quit", 10, 3 * 10);
+    gfx_PrintStringXY("/\\ , \\/ , 2nd - Swap Display mode", 10, 4 * 10);
+    gfx_PrintStringXY("< , > , alpha - Swap Timetable (A/B)", 10, 5 * 10);
+    gfx_PrintStringXY("clear - Swap Class (A/B)", 10, 6 * 10);
+    gfx_PrintStringXY("I - Show this page", 10, 7 * 10);
+
+    gfx_PrintStringXY("If week is 101, check that the", 10, 9 * 10);
+    gfx_PrintStringXY("date is correct. If not, quit and enter", 10, 10 * 10);
+    gfx_PrintStringXY("the correct date at mode > SET CLOCK", 10, 11 * 10);
 }
 
 void drawLesson(int16_t x, int16_t y, char *name, char *location, char *teacher, char *length, int8_t lessonColor, int8_t textColor, char *timeFrom, char *timeTo){
